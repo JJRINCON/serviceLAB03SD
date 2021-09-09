65cd25cc1a9d9ef140729f28874305476a8e172a
Jhon Jairo Rincon <48601559+JJRINCON@users.noreply.github.com>
2021-09-09 16:26:40 -0500

    Update university.sql

--- university.sql
+++ university.sql
@@ -14,6 +14,3 @@ INSERT INTO students VALUES (4, "Duvan", "Psicologia");
 INSERT INTO students VALUES (5, "Estefania", "Ingenieria ambiental");
 INSERT INTO students VALUES (6, "Raul", "Economia");
 
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
-
-flush privileges;
